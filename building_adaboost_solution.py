ba930c1b327b193664bde18277102da59f3d15e1
Jan Sowa <jan.piotr.sowa@gmail.com>
2019-11-15 12:42:58 +0100

    Add building adaboost solution with few categorical features

--- building_adaboost_solution.py
+++ building_adaboost_solution.py
@@ -1,28 +1,32 @@
-# Best AdaBoostRegressor error: 0.033773077400313094 for n_estimators = 20, desion tree max depth = 7, learning_rate = 1.4
-
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
+from preprocessing_data import prepare_categorical_features
 
 train_file_path = './train.csv'
-train_data = pd.read_csv(train_file_path)
-train_y = train_data.SalePrice
-features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd', 'OverallQual', 'OverallCond', 'MSSubClass', 'YearRemodAdd']
-
-train_X = train_data[features]
-
-regressor = AdaBoostRegressor(n_estimators=24, base_estimator=DecisionTreeRegressor(max_depth=20), learning_rate=1.55)
-print("Fitting regressor...")
-regressor.fit(train_X, train_y)
+train_X = pd.read_csv(train_file_path, index_col='Id')
+train_X.dropna(axis=0, subset=['SalePrice'], inplace=True)
+train_y = train_X.SalePrice
+train_X.drop(['SalePrice'], axis=1, inplace=True)
 
 test_data_path = './test.csv'
-test_data = pd.read_csv(test_data_path)
-test_X = test_data[features]
+test_X = pd.read_csv(test_data_path, index_col='Id')
+
+numerical_features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd',
+                      'OverallQual', 'OverallCond', 'MSSubClass', 'YearRemodAdd']
+categorical_features = ['MSZoning', 'ExterQual', 'CentralAir', 'KitchenQual']
+
+test_X.fillna({'KitchenQual': 'TA', 'MSZoning': 'RL'}, inplace=True)
+train_X, test_X = prepare_categorical_features(train_X, test_X, categorical_features, numerical_features)
+
+regressor = AdaBoostRegressor(n_estimators=26, base_estimator=DecisionTreeRegressor(max_depth=20), learning_rate=1.3)
+print("Fitting regressor...")
+regressor.fit(train_X, train_y)
 
 print("Predicting labels...")
 test_predictions = regressor.predict(test_X)
 
 print("Formatting data...")
-output = pd.DataFrame({'Id': test_data.Id,
+output = pd.DataFrame({'Id': test_X.index,
                        'SalePrice': test_predictions})
 output.to_csv('submission.csv', index=False)
